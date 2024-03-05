cbaa331ba7ded4457c5bd34c237d733672cafafc
Trevor Sullivan <trevorjohnsullivan@gmail.com>
2024-03-05 17:09:33 -0330

    In urls_index.ejs template, added a form element for each shortURL which uses a POST method and added a POST route for /urls/:id/delete to remove URLs

--- express_server.js
+++ express_server.js
@@ -20,6 +20,11 @@ function generateRandomString(length) {
 
 app.use(express.urlencoded({ extended: true }));
 
+app.post("/urls/:id/delete", (req, res) => {
+delete urlDatabase[req.params.id];
+res.redirect("/urls");
+})
+
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
