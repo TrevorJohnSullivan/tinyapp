2163d6871d988d7b803cbca508342b5dfe8086ed
Trevor Sullivan <trevorjohnsullivan@gmail.com>
2024-03-11 21:21:28 -0230

    updated testing file so that test wwork properly

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,7 +1,7 @@
 const { assert } = require('chai');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const getUserByEmail = require('../helpers.js');
+const { getUserByEmail } = require('../helpers.js');
 const expect = chai.expect;
 
 chai.use(chaiHttp);
@@ -39,6 +39,27 @@ describe('getUserByEmail', function() {
   });
 });
 
+
+
+
+// provded by instructions
+describe("Login and Access Control Test", () => {
+  it('should return 403 status code for unauthorized access to "http://localhost:8080/urls/b2xVn2"', () => {
+    const agent = chai.request.agent("http://localhost:8080");
+
+    // Step 1: Login with valid credentials
+    return agent
+      .post("/login")
+      .send({ email: "user2@example.com", password: "dishwasher-funk" })
+      .then((loginRes) => {
+        // Step 2: Make a GET request to a protected resource
+        return agent.get("/urls/b2xVn2").then((accessRes) => {
+          // Step 3: Expect the status code to be 403
+          expect(accessRes).to.have.status(403);
+        });
+      });
+  });
+});
 
 describe('URL Access Control Tests', () => {
     let agent;
@@ -83,23 +104,4 @@ describe('URL Access Control Tests', () => {
                 expect(res).to.have.status(403);
             });
     });
-});
-
-// provded by instructions
-describe("Login and Access Control Test", () => {
-  it('should return 403 status code for unauthorized access to "http://localhost:8080/urls/b2xVn2"', () => {
-    const agent = chai.request.agent("http://localhost:8080");
-
-    // Step 1: Login with valid credentials
-    return agent
-      .post("/login")
-      .send({ email: "user2@example.com", password: "dishwasher-funk" })
-      .then((loginRes) => {
-        // Step 2: Make a GET request to a protected resource
-        return agent.get("/urls/b2xVn2").then((accessRes) => {
-          // Step 3: Expect the status code to be 403
-          expect(accessRes).to.have.status(403);
-        });
-      });
-  });
 });
